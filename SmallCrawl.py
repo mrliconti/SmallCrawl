1ecff40438e541d5d29b67631df941d1005fda14
mrliconti <38948337+mrliconti@users.noreply.github.com>
2018-05-03 15:10:52 -0400

    Create SmallCrawl.py
    First attempt at creating the game.

---
+++ SmallCrawl.py
@@ -0,0 +1,43 @@
+# Mr. Liconti
+# May 3, 2018
+# SmallCrawl.py
+# Small Crawl: A small crawler game written in Python
+
+import random #will need for random events
+import numpy  #will need for joining 1D arrays into a 2D array
+
+######################################################################################
+# VARIABLES:
+
+#room types, loot types, encounter types
+room_bank = ['Hall' , 'Large Room', 'Small Room' , 'Library' , 'Conservatory' , 'Lab', 'Kitchen']
+loot_bank = ['coin', 'key', 'flashlight', 'health', 'weapon', 'armour']
+encounters_bank = ['BadGuy1', 'BadGuy2', 'BadGuy3', 'BadGuy4']
+
+# Empty Arrays
+map = []  #empty map array
+loot = [] #empty loot array
+encounters = [] #empty encounter array
+######################################################################################
+
+def generateLayout(someBank):
+  randomArray = random.sample(someBank, 4) #generate map, loot, or encounters. Depends on which bank was passed
+  return randomArray
+
+def showWhatWasGenerated():
+  print(map)  #proof that random map was generated
+  print(loot) # proof that random loot was generated
+  print(encounters) #proog that random encounters were generated
+
+def showBoard(b):
+  print(b)
+
+def makeBoard(m,l,e):
+  b = numpy.vstack([m, l, e])
+  return b
+  
+map = generateLayout(room_bank) #generate the map
+loot = generateLayout(loot_bank)  #generate the loot
+encounters = generateLayout(encounters_bank)  #generate the encounters
+board = makeBoard(map, loot, encounters)  #place the map, loot and encoutner arrays on the board
+showBoard(board)  #show the board
